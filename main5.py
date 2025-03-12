0dc0ae154172a571cb3a8e7bb5ea47a2208513d5
Ekaterina Ermak <ekaterina.ermak2006@gmail.com>
2025-03-12 13:01:29 +0700

    Добавление остальных функций

--- main5.py
+++ main5.py
@@ -55,3 +55,61 @@ def maclaurin_power_series(x, m, iterations=10):
         term = ((-1) ** n) * (comb(m, n) * (x ** n)) / factorial(n)
         result += term
     return result
+
+def maclaurin_cos(x, iterations=10):
+    """
+    Вычисляет значение косинуса с использованием ряда Маклорена.
+
+    :param x: Угол в радианах.
+    :param iterations: Количество итераций для вычисления.
+    :return: Значение косинуса.
+    :raises ValueError: Если количество итераций меньше 1.
+    :example: maclaurin_cos(1.0, 10)
+    """
+    if iterations < 1:
+        raise ValueError("Количество итераций должно быть больше 0.")
+    
+    result = 0
+    for n in range(iterations):
+        term = ((-1) ** n) * (x ** (2 * n)) / factorial(2 * n)
+        result += term
+    return result
+
+def maclaurin_ln1_plus_x(x, iterations=10):
+    """
+    Вычисляет значение ln(1+x) с использованием ряда Маклорена.
+
+    :param x: Значение x.
+    :param iterations: Количество итераций для вычисления.
+    :return: Значение ln(1+x).
+    :raises ValueError: Если x не в диапазоне (-1, 1].
+    :example: maclaurin_ln1_plus_x(0.5, 10)
+    """
+    if not (-1 < x <= 1):
+        raise ValueError("x должно быть в диапазоне (-1, 1].")
+    
+    result = 0
+    for n in range(1, iterations + 1):
+        term = (-1) ** (n + 1) * (x ** n) / n
+        result += term
+    return result
+
+def maclaurin_power_series_plus(x, m, iterations=10):
+    """
+    Вычисляет значение (1+x)^m с использованием ряда Маклорена.
+
+    :param x: Значение x.
+    :param m: Параметр степени.
+    :param iterations: Количество итераций для вычисления.
+    :return: Значение (1+x)^m.
+    :raises ValueError: Если x не в диапазоне (-1, 1).
+    :example: maclaurin_power_series_plus(0.5, 2, 10)
+    """
+    if not (-1 < x < 1):
+        raise ValueError("x должно быть в диапазоне (-1, 1).")
+    
+    result = 0
+    for n in range(iterations):
+        term = (comb(m, n) * (x ** n))
+        result += term
+    return result
